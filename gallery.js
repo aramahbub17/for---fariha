e7a7f3d4bce727bfaf959a741f537127c09132f3
aramahbub17 <mahbubalfa123@gmail.com>
2026-07-31 23:55:47 +0600

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,16 +1,18 @@
 const images = [
-    "file_00000000543c72078da120a8363a66fa",
-    "IMG_20260522_194521_539",
-    "IMG_20260719_222654_540",
-    "IMG_20250914_171346_690",
-    "IMG_20250331_154904_555"
+    "images/photo1.jpg",
+    "images/IMG_20250331_154904_555.jpg",
+    "images/IMG_20250914_171346_690.jpg",
+    "images/IMG_20260522_194521_539.jpg",
+    "images/IMG_20260719_222654_540.jpg",
+    "images/file_00000000543c72078da120a8363a66fa.png"
 ];
 
 const captions = [
-    "Happy Girlfriend Day, Amr Lokkhi Pakhii 💗",
+    "Happy Girlfriend Day, Kuchupuchu 💗",
     "You're my favorite person.",
     "Your smile is my favorite view.",
     "Thank you for being you. 🌸",
+    "You make every moment special. 💗",
     "I hope this little surprise makes you smile. 🤍"
 ];
 
