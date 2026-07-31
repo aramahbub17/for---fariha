8e00b6a809a5e934fa0c55ce092d49ef8798a287
aramahbub17 <mahbubalfa123@gmail.com>
2026-07-31 12:54:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function unlockPage() {
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("message");
 
+    if (password === "201017") {
+        message.style.color = "#00ff88";
+        message.innerHTML = "✅ Unlocking... ❤️";
+
+        setTimeout(function () {
+            window.location.href = "welcome.html";
+        }, 1500);
+
+    } else {
+        message.style.color = "#ff4d4d";
+        message.innerHTML = "❌ Wrong Password";
+    }
+}
