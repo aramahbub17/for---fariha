f301209370b41526a50c574b69afed974f680cb6
aramahbub17 <mahbubalfa123@gmail.com>
2026-07-31 21:22:52 +0600

    Update envelope.js

--- envelope.js
+++ envelope.js
@@ -1,1 +1,20 @@
+function openEnvelope() {
 
+    const envelope = document.querySelector(".envelope");
+
+    envelope.classList.add("open");
+
+    setTimeout(() => {
+
+        document.body.style.transition = "opacity 1s";
+        document.body.style.opacity = "0";
+
+    }, 1800);
+
+    setTimeout(() => {
+
+        window.location.href = "welcome.html";
+
+    }, 2800);
+
+}
