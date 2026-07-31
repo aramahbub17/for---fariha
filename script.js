89c050f16b32434fe1792241360a972803b44667
aramahbub17 <mahbubalfa123@gmail.com>
2026-08-01 00:48:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 function unlockPage() {
     let password = document.getElementById("password").value;
     let message = document.getElementById("message");
+    let surprise = document.getElementById("surprise");
 
     if (password === "201017") {
+
         message.style.color = "#00ff88";
-        message.innerHTML = "Unlocking...";
+        message.innerHTML = "Unlocked 🤍";
 
         setTimeout(function () {
-            window.location.href = "envelope.html";
-        }, 1500);
+            surprise.style.display = "block";
+            document.querySelector(".lock-container").scrollIntoView({
+                behavior: "smooth"
+            });
+        }, 800);
 
     } else {
+
         message.style.color = "#ff4d4d";
         message.innerHTML = "❌ Wrong Password";
+
     }
 }
